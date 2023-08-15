14dc444005e1f004bae2fff3532432c375622683
Mohamed_Amin_Hajbi <aminhajbi116@gmail.com>
2023-08-15 16:38:28 +0100

    Directive ngIf

--- first-app/src/app/first/first.component.ts
+++ first-app/src/app/first/first.component.ts
@@ -11,6 +11,7 @@ export class FirstComponent {
   age = 23;
   imageURL = "https://imgv3.fotor.com/images/blog-cover-image/part-blurry-image.jpg";
   text = "";
+  display = false;
   change(){
     this.age=20;
     this.name="Mohamed";
